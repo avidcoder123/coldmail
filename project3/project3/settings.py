b2e94b24fe969acf519d053c39160979671c7f63
thomcatmeow <60586376+thomcatmeow@users.noreply.github.com>
2020-07-17 12:53:26 -0500

    Update settings.py

--- project3/project3/settings.py
+++ project3/project3/settings.py
@@ -120,5 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
